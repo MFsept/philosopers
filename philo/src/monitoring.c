db5c2df1a6839fa2d19b53ec8876bb43a0126bcd
Matteo Fernandes <mfernand@f2r11s10.clusters.42paris.fr>
2025-06-10 11:08:47 +0200

    fix died last msg

--- philo/src/monitoring.c
+++ philo/src/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: mfernand <mfernand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 14:49:07 by mfernand          #+#    #+#             */
-/*   Updated: 2025/06/09 21:52:30 by mfernand         ###   ########.fr       */
+/*   Updated: 2025/06/10 11:07:59 by mfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,11 @@ static int	check_philos(t_info *info, int *philo_full)
 		time_hungry = get_time_ms() - info->philo[i].last_meal;
 		if (time_hungry > info->ttd)
 		{
-			safe_printf(&info->philo[i], "died");
 			pthread_mutex_lock(&info->end_mutex);
 			info->end = TRUE;
+			safe_handle_mutex(&info->print_mutex, LOCK);
+			printf("%li %i died\n", get_time_ms(), info->philo[i].id);
+			safe_handle_mutex(&info->print_mutex, UNLOCK);
 			pthread_mutex_unlock(&info->end_mutex);
 			pthread_mutex_unlock(&info->philo[i].meal_mutex);
 			return (1);
