59695597f1f0c828ed5ab48ff8bdbedb192ec95c
Matteo Fernandes <mfernand@f2r10s1.clusters.42paris.fr>
2025-06-12 16:49:26 +0200

    fix atol

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,79 +6,62 @@
 /*   By: mfernand <mfernand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 12:09:15 by mfernand          #+#    #+#             */
-/*   Updated: 2025/06/10 16:02:51 by mfernand         ###   ########.fr       */
+/*   Updated: 2025/06/12 16:41:32 by mfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	ft_is_digit(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			i++;
-		else
-			return (FALSE);
-	}
-	return (TRUE);
-}
-
-static int	ft_is_space(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] >= 9 && s[i] <= 13)
-			i++;
-		else
-			return (FALSE);
-	}
-	return (TRUE);
-}
-
 char	*valid_input(char *s)
 {
-	int		len;
-	char	*valid_nb;
-	int		i;
-
+    int		len;
+    int		i;
+	int		start;
+	
 	i = 0;
 	len = 0;
-	while (ft_is_space(s))
-		i++;
-	if (s[i] == '+')
-		i++;
-	else if (*s == '-')
-		return (printf("The number must be positive\n"), NULL);
-	else if (!ft_is_digit(s))
-		return (printf("Only digit characters are accepted\n"), NULL);
-	valid_nb = s + i;
-	while ((s[i] >= '0' && s[i++] <= '9'))
-		len++;
-	if (len > 10)
-		return (printf("The number is bigger than INT_MAX\n"), NULL);
-	return (valid_nb);
+	start = 0;
+
+    while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+        i++;
+    if (s[i] == '+')
+        i++;
+    else if (s[i] == '-')
+        return (printf("The number must be positive\n"), NULL);
+    if (!(s[i] >= '0' && s[i] <= '9'))
+        return (printf("Only digit characters are accepted\n"), NULL);
+    start = i;
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        len++;
+        i++;
+    }
+    while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+        i++;
+    if (s[i] != '\0')
+        return (printf("Only digit characters are accepted\n"), NULL);
+    if (len > 10)
+        return (printf("The number is bigger than INT_MAX\n"), NULL);
+    return (s + start);
 }
 
 long	ft_atol(char *s)
 {
-	long	result;
+    long	result = 0;
+    char	*nb = valid_input(s);
 
-	result = 0;
-	if (!valid_input(s))
-		return (0);
-	while (*s >= '0' && *s <= '9')
-		result = result * 10 + (*s++ - 48);
-	if (result > INT_MAX)
-		return (printf("The number is not an integer\n"), 0);
-	return (result);
+    if (!nb)
+        return (0);
+    while (*nb >= '0' && *nb <= '9')
+    {
+        result = result * 10 + (*nb - '0');
+        if (result > INT_MAX)
+            return (printf("The number is not an integer\n"), -1);
+        nb++;
+    }
+    return (result);
 }
+
 
 int	ttk(t_philo *philo)
 {
